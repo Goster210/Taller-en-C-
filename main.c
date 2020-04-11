7a82d8dbfbb3affb93b40a6157729d3e3c030239
Goster210 <63003580+Goster210@users.noreply.github.com>
2020-04-11 17:31:33 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,39 +1,112 @@
 #include <stdio.h>
 
-int calculateMCD( int less, int higher ){
-
-}
-
+//---- SECUENCIA FIBONACCI ---- ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 void calcFibonacci(){
     printf( "Calcular Termino de la Serie de Fibonacci\nENTER Para Continuar" );
     getchar();
 }
 
+//---- MAXIMO COMUN DIVISOR ---- //////////////////////////////////////////////////////////////////////////////////////////////////////////
 void calcMCD(){
     printf( "Calcular Maximo Comun Divisor\nENTER Para Continuar" );
     getchar();
 }
+//---- SI SON NUMEROS PRIMOS ----//////////////////////////////////////////////////////////////////////////////////////////////////////////
+void primeNumbers(){
+	printf( "\n----CALCULO DE NUMEROS PRIMOS----\n\n" );
+	int number;
+	printf("DIGITA EL NUMERO\n");
+	scanf("%i",&number);
+    calculation(number);
+    printf("\nENTER Para Continuar" );
+    getchar();
+    getchar();
+}
 
+int calculation (int num){
+	int cont=0;
+	if(num>1){	
+	int i=1;
+	for(i;i<=num;i++){
+	if(num%i==0){
+		cont++;
+	}
+	}
+	if(cont>2){
+	cont==0;
+	}else{
+	printf("El numero [%i] es primo \n",num);
+	cont==0;
+	}
+	calculation(num-1);
+	}
+}
+//---- SI SON NUMEROS AMIGOS ---- /////////////////////////////////////////////////////////////////////////////////////////////////////////
+void friendsNumbers(){
+	printf( "\n----CALCULO DE NUMEROS AMIGOS----\n\n" );
+	int number1,number2;
+	 printf("Digita el numero 1\n");
+	 scanf("%i",&number1);
+	 printf("Digita el numero 2\n");
+	 scanf("%i",&number2);
+     validate(number1,number2,(calculate(number1)),(calculate(number2)));
+       printf("\nENTER Para Continuar" );
+    getchar();
+    getchar();
+} 
+
+int calculate (int number){
+	 int count=0;
+	 int  i;
+	 for (i=1; i<number;i++){
+	 if((number%i)==0){
+	 	count=count+i;	
+	    }
+	 }
+	return count;
+}
+int validate (int number1,int number2,int calculatenumber1,int calculatenumber2){
+	if(number1 == calculatenumber2 && number2 == calculatenumber1){
+		 printf("El numero [%i] y el numero [%i] SI son numeros amigos\n",number1,number2);
+	}else{
+	     printf("El numero [%i] y el numero [%i] NO son numeros amigos\n",number1,number2);
+	}
+}
+//---- SI EL NUMERO ES CAPICUA ---- ///////////////////////////////////////////////////////////////////////////////////////////////////////
+void numberCapicua(){
+   
+    getchar();
+}
+//---- SI LA PALABRA ES PALINDROMO ---- ///////////////////////////////////////////////////////////////////////////////////////////////////
+void palindromo(){
+    getchar();
+}
+//---- AÑO BISIESTO ---- /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void calBisiesto() {
     int year = 0;
-    do  {
-        printf("\nDijite el año que desea saber si es bisiesto o no ", year);
+  
+        printf("\nDijite el año que desea saber si es bisiesto o no\n", year);
         scanf("%d", &year);
         if ((year % 4 == 0 && year % 100 != 0)||year%400==0) {
             printf("El año %d es bisiesto", year);
         } else {
-            printf("El año %d no es bisiesto", year);
+            printf("El año %d no es bisiesto\n", year);
         }
-    } while ((year > 0)) ;
+      printf("\nENTER Para Continuar" );
+    getchar();
+    getchar();
 }
-
+//---- FIN ---- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void mainMenu(){
     char option;
 
-    char *mainMenu = "<<<<MENU PRINCIPAL>>>>\n\n"
+    char *mainMenu = "\n<<<<MENU PRINCIPAL>>>>\n\n"
 	"1. Serie de Fibonnaci\n"
     "2. Maximo Comun Divisor\n"
     "3. Numeros Primos\n"
+    "4. Numeros Amigos\n"
+    "5. Numero Capicua\n"
+    "6. Palindromo\n"
     "7. Año bisiesto\n\n"
     "Digite Opcion ([s/S]-Salir).... ";
     do{
@@ -46,8 +119,20 @@ void mainMenu(){
 
             case '2' : calcMCD();
             break;
+            
+            case '3' : primeNumbers();
+            break;
+            
+            case '4': friendsNumbers();
+            break;
+            
+            case '5': numberCapicua;
+            break;
+            
+            case '6': palindromo();
+            break;
 			
-	    case '7': calBisiesto();
+	        case '7': calBisiesto();
             break;
         }
 
